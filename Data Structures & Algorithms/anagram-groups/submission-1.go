26ecbcff5e71334845f355d57e76fee8510aecc7
reshamsanthoshs <reshamsanthoshs@gmail.com>
2026-07-10 20:53:51 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.go
@@ -0,0 +1,19 @@
+func groupAnagrams(strs []string) [][]string {
+   groups := make(map[[26]int][]string)
+   for _, word := range strs {
+        var count [26]int
+        for _, ch := range word {
+            count[ch-'a']++
+
+        }
+        groups[count] = append(groups[count], word)
+    }
+    result := [][]string{}
+
+    for _, group := range groups {
+        result = append(result, group)
+    }
+    return result
+}
+
+
